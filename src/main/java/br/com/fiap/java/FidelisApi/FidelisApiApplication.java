067bdd56053c3bb4ce84d8b74913a6e299890f12
Henrique <henriquect08@gmail.com>
2026-05-19 17:27:01 -0300

    feat: configuração inicial do projeto Spring Boot

---
+++ src/main/java/br/com/fiap/java/FidelisApi/FidelisApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.java.FidelisApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidelisApiApplication.class, args);
+	}
+
+}
